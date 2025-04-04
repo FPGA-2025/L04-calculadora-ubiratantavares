e41b665023bed9d76d9087777a21922d85cbb822
Ubiratan Tavares <ust1973@gmail.com>
2025-04-04 19:14:03 -0300

    implementando a calculadora.v

--- calculadora.v
+++ calculadora.v
@@ -1,10 +1,15 @@
-module calculadora(
-  input   [7:0] entrada_A,
-  input   [7:0] entrada_B,
-  input   [2:0] codigo,
-  output  [7:0] saida
+module calculadora (
+  input  [7:0] entrada_A,
+  input  [7:0] entrada_B,
+  input  [2:0] codigo,
+  output [7:0] saida
 );
 
-// insira seu código aqui
-
+  assign saida = (codigo == 3'b000) ? 8'b00000000 :             // zerar
+                 (codigo == 3'b001) ? entrada_A   :             // mostrar_A
+                 (codigo == 3'b010) ? entrada_B   :             // mostrar_B
+                 (codigo == 3'b011) ? entrada_A + entrada_B :   // somar
+                 (codigo == 3'b100) ? entrada_A - entrada_B :   // subtrair
+                 8'b00000000;  // default (operação inválida)
 endmodule
+
